5e3e1d73e83c73880868d153a5355f24238554d7
Dusanzun <64102283+Dusanzun@users.noreply.github.com>
2020-05-09 22:38:26 +0200

    Init
    Initial Commit

---
+++ ACMo/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61ac2f6e-5336-4d32-8ea1-e952ede4b97c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ACMo/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ACM.BLi;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {   //--Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+            string expected = "Baggins, Bilbo";
+            //--Act
+            string actual = customer.FullName;
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
