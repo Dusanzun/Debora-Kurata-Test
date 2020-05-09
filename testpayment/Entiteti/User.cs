1c545cbaaca60b831f7b51df9887df420a3dc79c
Dusanzun <64102283+Dusanzun@users.noreply.github.com>
2020-05-10 00:05:34 +0200

    Initial Commit

---
+++ testpayment/Entiteti/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testpayment.Entiteti
+{
+    public class User
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+
+
+    }
+}
